1369a4f2382feb77f8ba3a424c6329b5711953bf
Yaroslav Kulkov <y_mail@mail.ru>
2026-03-06 16:30:13 +0300

    test in main

--- hw01/src/main/java/ru/kulkov/equation/Main.java
+++ hw01/src/main/java/ru/kulkov/equation/Main.java
@@ -1,10 +1,12 @@
 package ru.kulkov.equation;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Solver solver = new EquationSolver();
         System.out.println(((EquationSolver) solver).findDiscriminant(1, 2.0000000000125, 1));
-//        System.out.println(Arrays.toString(solver.solve(1, 2.0000000000125, 1)));
+        System.out.println(Arrays.toString(solver.solve(1, 2.0000000000125, 1)));
 
         System.out.println(((EquationSolver) solver).findDiscriminant(1, Double.MAX_VALUE, 1));
         //System.out.println(Arrays.toString(solver.solve(1, Double.MAX_VALUE, 1)));
