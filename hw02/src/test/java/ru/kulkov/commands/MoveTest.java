b4665a625a79374ab66b5e4f30497d549d482cdc
Yaroslav Kulkov <y_mail@mail.ru>
2026-04-23 17:23:12 +0300

    Created test with mock

--- hw02/src/test/java/ru/kulkov/commands/MoveTest.java
+++ hw02/src/test/java/ru/kulkov/commands/MoveTest.java
@@ -2,11 +2,18 @@ package ru.kulkov.commands;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import ru.kulkov.api.Movable;
+import ru.kulkov.entities.UObject;
 import ru.kulkov.geometry.Point;
 import ru.kulkov.geometry.Velocity;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class MoveTest {
@@ -21,5 +28,25 @@ class MoveTest {
         Point expected = new Point(5, 8);
         assertEquals(expected.getX(), actual.getX());
         assertEquals(expected.getY(), actual.getY());
+    }
+
+    @Test
+    void moveShouldUpdateLocationUsingVelocity(@Mock UObject uObject) {
+        Point startLocation = new Point(12, 5);
+        Velocity velocity = new Velocity(-7, 3);
+
+        when(uObject.getProperty("location", Point.class)).thenReturn(startLocation);
+        when(uObject.getProperty("velocity", Velocity.class)).thenReturn(velocity);
+
+        Movable movable = new MovingObjectAdaptor(uObject);
+        Move moveCommand = new Move(movable);
+        moveCommand.execute();
+
+        ArgumentCaptor<Point> pointCaptor = ArgumentCaptor.forClass(Point.class);
+        verify(uObject).setProperty(eq("location"), pointCaptor.capture());
+
+        Point actual = pointCaptor.getValue();
+        assertEquals(5, actual.getX());
+        assertEquals(8, actual.getY());
     }
 }
